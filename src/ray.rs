fa497ac0c8a572e44541138dfdbd23bd1f7e2fd3
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-07-19 16:47:02 -0700

    add ray code

---
+++ src/ray.rs
@@ -0,0 +1,36 @@
+use nalgebra as na;
+
+use crate::scene::Scene;
+
+pub struct Ray<T: na::RealField> {
+    pub origin: na::Point3<T>,
+    pub direction: na::Vector3<T>,
+}
+
+fn to_radians<T: na::RealField>(a: T) -> T {
+	a * T::pi() / na::convert(180.0)
+}
+
+impl<T: na::RealField> Ray<T> {
+	pub fn new_prime(x: u32, y: u32, scene: &Scene<T>) -> Ray<T> {
+		let Scene { width, height, fov, .. } = *scene;
+		assert!(width > height, "width must be greater than height to prevent distortion (for now)");
+
+		let (width, height) = (T::from_u32(width).unwrap(), T::from_u32(height).unwrap());
+		let half = na::convert(0.5);
+		let two = na::convert(2.0);
+
+		let aspect = width / height;
+		let fov_adj = (to_radians(fov) / two).tan();
+		
+		let x = T::from_u32(x).unwrap();
+		let sensor_x = (((x + half) / width) * two - T::one()) * aspect * fov_adj;
+		let y = T::from_u32(y).unwrap();
+		let sensor_y = T::one() - ((y + half) / height) * two;
+
+		Ray {
+			origin: na::Point3::origin(),
+			direction: na::Vector3::new(sensor_x, sensor_y, -T::one()).normalize(),
+		}
+	}
+}
